607323447c2efc7fa6958687d22cf82387dd6ae2
DizzyRaven <akaynt47@gmail.com>
2020-04-16 22:31:47 +0300

    improve swagger doc

--- DeliverySystem/Startup.cs
+++ DeliverySystem/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.IO;
 using System.Reflection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace DeliverySystem
 {
@@ -47,7 +48,7 @@ namespace DeliverySystem
 
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Food Delivery System V1");
             });
 
             app.UseHttpsRedirection();
@@ -66,7 +67,25 @@ namespace DeliverySystem
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info() { Title = "Delivery System" });
+                c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info()
+                {
+                    Title = "Delivery System",
+                    Description = "Food delivery system with role-separated functionality",
+                    TermsOfService = "https://example.com/",
+                    Version = "V1.0.0.5",
+                    Contact = new Contact()
+                    {
+                        Name = "Illia Bondar",
+                        Email = "i.bondar@ukma.edu.ua",
+                        Url = "https://www.linkedin.com/in/%D0%B8%D0%BB%D1%8C%D1%8F-%D0%B1%D0%BE%D0%BD%D0%B4%D0%B0%D1%80%D1%8C-9825b1152/",
+                    },
+                    License = new License()
+                    {
+                        Name = "Use under LICX",
+                        Url = "https://example.com/license",
+                    }
+
+                });
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
